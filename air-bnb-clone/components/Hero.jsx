6c3b6973f213d461cc6ca24c06c698dbc666592a
Frenzy017 <aleksladjov4@gmail.com>
2023-04-29 10:43:49 +0300

    Update file

---
+++ air-bnb-clone/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="../images/herophoto.png" className="hero--photo"/>
+            <h1 className="hero--photo">Online Experiences</h1>
+            <p className="hero--photo">
+                Join unique interactive activities led by one-of-a-kind hosts—all
+                without leaving home.
+            </p>
+        </section>
+    );
+}
---
+++ air-bnb-clone/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./components/Navbar.jsx", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
